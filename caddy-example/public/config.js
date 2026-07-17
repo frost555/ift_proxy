c36301976e4de413e9ddda8d589a15a309022a2c
Igor Okorokov <frost555@gmail.com>
2026-07-17 13:29:10 +0300

    add static files

---
+++ caddy-example/public/config.js
@@ -0,0 +1,166 @@
+var vtb24Config = {
+  "oldIbAddress": "https://online.if.test.vtb.ru/",
+  "epaEnabled": true,
+  "epaHeader": "parb_ift_ib@test.vtb.ru",
+  "otpTestHeader": true,
+  "apiKeyYandexCheckLinks": "c017b274-9830-40f6-b94b-a79a0f8310fd",
+  "checkLinksUrl": "https://sfd.fzlabs.ru:5430 https://sba.yandex.net https://p0esmd-mo5405lv.test.vtb.ru:9999",
+  "webChat": {
+    "disabled": false,
+    "channel_id": 13,
+    "unauthorized_disabled": false,
+    "unauthorized_channel_id": 2009,
+    "readonly_channel_id": 43,
+    "lite_disabled": false,
+    "lite_channel_id": 488,
+    "webchatserverUrl": "https://ift-chat.vtb.ru/widget/loader.js",
+    "host": "ift-chat.vtb.ru",
+    "protocol": "https"
+  },
+  "asyncServer":{
+    "sse": "https://ift-ibrb1-sharing.vtb.ru/sse/sse",
+    "socket": "wss://ift-ibrb1-sharing.vtb.ru/sse/websocket"
+  },
+  "authSSO":{
+    "WSO2": "",
+    "AUTH_TOKEN": "Basic QzJWWXYzYjZSSEVpZzJuXzU2YmZubjNHZkk0YTpWaXFLSG9fTXRSYm05bFNTeVJGQ1hmTnRDblFh",
+    "TG_BANK_AUTH_TOKEN": "Basic bE9XZ21IMUVuZ21zUU10eVJPaUdoa1ZndmpJYTpwTHRtbWpKNEdnWHV1eXFMeGVsUmc3MktDdlFh",
+    "VK_BANK_AUTH_TOKEN": "Basic YTFhNTBhZjUtMzAyYS00ZGI0LWJmOTUtZTViNDZjNDhiNmIxOjkwNTk2MjE5LWRjZGUtNDgzOC05NzU2LTYzNGM1NTkwNGQ1OQ==",
+    "FSO_ONBOARDING_AUTH_TOKEN": "Basic ZjQzMDM5NjEtNTI5My00NjlhLWFhOTMtOTRiOTRiYTE2MTNhOmUwMTQxNTYxLTRlOGItNDBkMi1hZjNkLTY2NmY3Y2VlZDI2MA==",
+    "ONBOARDING_CAPTCHA_TOKEN": "ysc1_2RPtg6LaDhaIZUW21D8P3oHuiaXuyT0Sth0pmGCs38bc02a6"
+  },
+  "integrationServer":{
+    "url": "/"
+  },
+  "coreSettingsBackendMockUrl": "",
+  "coreSettingsVersion": "",
+  "forceUseFocusGroupCheck": false,
+  "allowFunctionSettings": true,
+  "cmsUrl": "https://h.if.vtb.ru",
+  "mobileBankServerUrl": "https://mb.if.test.vtb.ru",
+  "pfrServerUrl": "https://online.if.test.vtb.ru/",
+  "debitCards":{
+    "ssoKey": "OBCqXU9Lavwra4yJLuRsV4I2uHka",
+    "ssoSecret": "S3tmBAailCKo7Il3BcQ2ZHx9LfMa",
+    "transactionType": "87",
+    "yandexMapKey": "e39c76e5-f3eb-4169-be99-244ac42db53b",
+    "healthcheckEka": "dks_questionnaire_healthcheck"
+  },
+  "salaryForm":{
+    "sso":{
+      "key": "4PxQYotKmwObTfueuQtdHqljDIsa",
+      "secret": "LmvuATiN9mTFzUjoUs9skOdYp7Ea"
+    }
+  },
+  "vipPrimeAnketa":{
+    "sso":{
+      "key": "Fb6w16MmjUa7T_90cbrmXtZhz3Qa",
+      "secret": "NfF6KzHh7fbinliUouJdgbt6wWEa"
+    },
+    "yandexcaptcha": {
+      "clientkey": "ysc1_2RPtg6LaDhaIZUW21D8P3oHuiaXuyT0Sth0pmGCs38bc02a6",
+    }
+  },
+  "messengers":{
+    "messengersKeyTimeDelta": "600",
+    "messengersPaymoRestApiUri": "https://testgate4.paymo.ru",
+    "messengersPaymoFrontRedirectUri": "https://5000.levkin.titanvm.paymo.ru"
+  },
+  "recaptcha":{
+    "checkboxKey": "6Le_yFUdAAAAAIXcmnP_-U9oLytwP2saIQQC7PSL",
+    "scoreBasedKey": "6Le8FlUdAAAAAAgA1wgln8QdF2Nz3tndoQI9HOy1"
+  },
+  "groupIBServerURL": "",
+  "groupIBHTMLServerURL": "",
+  "injectCoreSettings": true
+}
+
+var vtbConfig = {
+  "webPush": {
+    "apiKey": "AIzaSyCB1bXdjrhOVy7cVmx9iCCcIpgcTT1j__0",
+    "authDomain": "bankvtb-219808.firebaseapp.com",
+    "databaseURL": "https://bankvtb-219808.firebaseio.com",
+    "projectId": "bankvtb-219808",
+    "storageBucket": "bankvtb-219808.appspot.com",
+    "messagingSenderId": "507996965485",
+    "appId": "",
+    "vapidKey": "BCGUMk4eKEwd0qqs5bROS6xItkT5fnhtITfTQ08Gighpn-AKoh40Bf6bgNU7lsdtv9SfK6s6fYRP510_hgfoPG0",
+    "providerUid": "PH5Ca0RIPUJqdSpsMGVJSFtAPC0oIkIuZUBMQFAyXER+Pg==",
+    "appPackage": "online.if.test.vtb.ru"
+  },
+  webPushUnp: {
+    apiKey: "AIzaSyCB1bXdjrhOVy7cVmx9iCCcIpgcTT1j__0",
+    authDomain: "bankvtb-219808.firebaseapp.com",
+    databaseURL: "https://bankvtb-219808.firebaseio.com",
+    projectId: "bankvtb-219808",
+    storageBucket: "bankvtb-219808.appspot.com",
+    messagingSenderId: "507996965485",
+    appId: "1:507996965485:web:58362a4154ac5b98090d64",
+    measurementId: "G-TZXMMX70XM",
+    vapidKey: "BCGUMk4eKEwd0qqs5bROS6xItkT5fnhtITfTQ08Gighpn-AKoh40Bf6bgNU7lsdtv9SfK6s6fYRP510_hgfoPG0",
+  },
+  "webChat": {
+    "webchatserverUrl": "https://ift-chat.vtb.ru/widget/loader.js",
+    "host": "ift-chat.vtb.ru",
+    "protocol": "https"
+  },
+  "asyncServer":{
+    "sse": "https://ift-ibrb1-sharing.vtb.ru/sse/sse",
+    "socket": "wss://ift-ibrb1-sharing.vtb.ru/sse/websocket"
+  },
+  "authSSO":{
+    "WSO2": "https://ift-ibrb1-sharing.vtb.ru"
+  },
+  "cmsUrl": "https://h.if.vtb.ru"
+}
+
+var vtbRFConfig = {
+  "webChat": {
+    "webchatserverUrl": "https://ift-chat.vtb.ru/widget/loader.js",
+    "host": "ift-chat.vtb.ru",
+    "protocol": "https"
+  },
+  "asyncServer":{
+    "sse": "https://ift-ibrb1-sharing.vtb.ru/sse/sse",
+    "socket": "wss://ift-ibrb1-sharing.vtb.ru/sse/websocket"
+  },
+  "authSSO":{
+    "WSO2": "https://ift-ibrb1-sharing.vtb.ru"
+  },
+  "cmsUrl": "https://h.if.vtb.ru"
+}
+
+function checkHostname(hostname) {
+  return window.location.hostname.toLowerCase().indexOf(hostname) !== -1;
+}
+
+function merge() {
+  var result = {},
+      src,
+      key,
+      args = [].splice.call(arguments, 0);
+  while (args.length > 0) {
+    src = args.splice(0, 1)[0];
+    if (toString.call(src) === "[object Object]") {
+      for (key in src) {
+        if (src.hasOwnProperty(key)) {
+          if (toString.call(src[key]) === "[object Object]") {
+            result[key] = merge(result[key] || {}, src[key]);
+          } else {
+            result[key] = src[key];
+          }
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+window._VTB = vtb24Config;
+
+if (checkHostname("vtb.ru")) {
+  window._VTB = merge(vtb24Config, vtbConfig);
+} else if (checkHostname("xn--90ab2c.xn--p1ai")) {
+  window._VTB = merge(vtb24Config, vtbRFConfig);
+}
